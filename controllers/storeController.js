e4ae53b7a457c452de860f716e7be23a83c52d5f
Ron Raithatha <ronaksraithatha@gmail.com>
2017-05-23 13:27:55 -0400

    added flash message to create store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -3,15 +3,18 @@ const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
   res.render('index');
-}
+};
 
 exports.addStore = (req, res) => {
-  res.render('editStore', {title: '💩 Add Store'});
-}
+  res.render('editStore', { title: '💩 Add Store' });
+};
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
+  const store = await new Store(req.body).save();
+  req.flash(
+    'success',
+    `Successfully created ${store.name}. Care to leave a review?`,
+  );
   console.log('It Worked');
-  res.redirect('/');
-}
+  res.redirect(`/store/${store.slug}`);
+};
