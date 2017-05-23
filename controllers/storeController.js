f68d8156f6f266df1fb5e2d76c53fb70a7a85f56
Ron Raithatha <ronaksraithatha@gmail.com>
2017-05-23 14:48:11 -0400

    added ability to create and update store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -24,3 +24,34 @@ exports.getStores = async (req, res) => {
   const stores = await Store.find();
   res.render('stores', { title: 'Stores', stores });
 };
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store given the id
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. Confirm they are the owner of the store
+  // 3. Render edit form so user can update store
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  // find and update store
+  const params = {
+    q: { _id: req.params.id },
+    data: req.body,
+    options: {
+      new: true, // return the new store instead of the old own
+      runValidators: true,
+    },
+  };
+  const store = await Store.findOneAndUpdate(
+    params.q,
+    params.data,
+    params.options
+  ).exec();
+  req.flash(
+    'success',
+    `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`
+  );
+  res.redirect(`/stores/${store._id}/edit`);
+  // redirect to store and tell them it worked
+};
