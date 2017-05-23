e11534834ba832c702745f6154c28bfa79af0bd9
Ron Raithatha <ronaksraithatha@gmail.com>
2017-05-23 00:18:19 -0400

    Added model and synchronous save

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,8 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 
-// Do work here
-// router.get('/', (req, res) => {
-//   // res.send('Hey! It works!');
-//   res.render('hello', {
-//     name: "wes",
-//     dog: "poa"
-//   })
-// });
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
 module.exports = router;
