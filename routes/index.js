a218898ac68610e81cf3ceaafc1111bb33d9a1cf
Ron Raithatha <ronaksraithatha@gmail.com>
2017-05-22 23:34:58 -0400

    added model and removed dist files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
-});
+// router.get('/', (req, res) => {
+//   // res.send('Hey! It works!');
+//   res.render('hello', {
+//     name: "wes",
+//     dog: "poa"
+//   })
+// });
+router.get('/', storeController.homePage);
 
 module.exports = router;
